1d66bda7be1cc59fa9c460a6c71578ba7eafc585
Soam <soam.champ@gmail.com>
2021-02-23 16:56:26 +0530

    Update basic_visualRegression.spec.js

--- cypress/tests/basic_visualRegression.spec.js
+++ cypress/tests/basic_visualRegression.spec.js
@@ -2,11 +2,6 @@
 describe('Visuals', () => {
     it('should compare screenshot from a given element', () => {
       cy.visit('www.google.com')
-      //cy.wait(30000)
-      cy.get('body img').compareSnapshot('google imgagaaaaa')
-
-     //cy.compareSnapshot('google imgagaaaaa', 0.0);
+      cy.get('body img').compareSnapshot('google imgage')
     })
   })
-
-  //https://assets.inshorts.com/website_assets/images/logo_inshorts.png
